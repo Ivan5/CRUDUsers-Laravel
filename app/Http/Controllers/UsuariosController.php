350823f49c6adabf1ea62b6effd057de1ade5e17
Ivan5 <converse_ivan@hotmail.com>
2018-07-19 16:01:27 -0500

    Add Store function to save user in db

--- app/Http/Controllers/UsuariosController.php
+++ app/Http/Controllers/UsuariosController.php
@@ -38,7 +38,21 @@ class UsuariosController extends Controller
     public function store(Request $request)
     {
         //
-        return 'Enviado';
+        $this->validate($request, [
+            'nombre' => 'required',
+            'email' => 'required',
+            'telefono' => 'required'
+        ]);
+
+        //Crear el usuario
+        $usuario = new Usuario;
+        $usuario->nombre = $request->input('nombre');
+        $usuario->email = $request->input('email');
+        $usuario->telefono = $request->input('telefono');
+
+        $usuario->save();
+        return redirect('/')->with('success','Usuario Agregado');
+
     }
 
     /**
